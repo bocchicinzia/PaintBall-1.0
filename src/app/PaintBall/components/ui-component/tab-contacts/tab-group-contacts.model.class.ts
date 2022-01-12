1272cf77fb86812c2d07278dba50749db2b8272d
ahmedRi94 <amedin94@live.it>
2022-01-12 19:15:30 +0100

    added class model

---
+++ src/app/PaintBall/components/ui-component/tab-contacts/tab-group-contacts.model.class.ts
@@ -0,0 +1,11 @@
+export class TabGroupContacts {
+  label: string;
+  path: string;
+  alt: string;
+
+  constructor( l: string, p: string, alt: string ) {
+    this.label = l;
+    this.path = p;
+    this.alt = alt;
+  }
+}
