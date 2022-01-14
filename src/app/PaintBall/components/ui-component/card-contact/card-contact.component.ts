ec3a6746a93d62624a4c269ecea4b0ff48e2c7dc
ahmedRi94 <amedin94@live.it>
2022-01-14 17:28:10 +0100

    WIP added functionallity and print icon

--- src/app/PaintBall/components/ui-component/card-contact/card-contact.component.ts
+++ src/app/PaintBall/components/ui-component/card-contact/card-contact.component.ts
@@ -1,15 +1,27 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, Input, OnInit } from '@angular/core';
+import { Observable } from 'rxjs';
+import { ContactsPageModel } from 'src/app/PaintBall/pages/contacts-page/contacts-page-model';
+import { CardContact } from './card-contact-model.class';
 
-@Component({
+@Component( {
   selector: 'app-card-contact',
   templateUrl: './card-contact.component.html',
   styleUrls: ['./card-contact.component.scss']
-})
+} )
 export class CardContactComponent implements OnInit {
+  @Input() selectedIndex: number;
+  @Input() cardContents: Observable<ContactsPageModel>;
+  content: CardContact[];
 
-  constructor() { }
+  constructor() {}
 
   ngOnInit(): void {
+    this.cardContents.subscribe( res => {
+      this.content = res.contentCard
+      console.log( this.content );
+    } );
+
+    this.selectedIndex = 0;
   }
 
 }
