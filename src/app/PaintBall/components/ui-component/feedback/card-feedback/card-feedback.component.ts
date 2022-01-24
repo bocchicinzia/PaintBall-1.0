b1b213ce9bfa079facc4f12db6632c6683c985fc
ahmedRi94 <amedin94@live.it>
2022-01-24 13:24:45 +0100

    added print star in card feedback

--- src/app/PaintBall/components/ui-component/feedback/card-feedback/card-feedback.component.ts
+++ src/app/PaintBall/components/ui-component/feedback/card-feedback/card-feedback.component.ts
@@ -7,12 +7,16 @@ import { CardFeedbackFirebasePrint } from '../feedback.class';
   styleUrls: ['./card-feedback.component.scss']
 } )
 export class CardFeedbackComponent implements OnInit {
-
+  checked: number;
   @Input() item: CardFeedbackFirebasePrint;
 
   constructor() {}
 
   ngOnInit(): void {
+    this.checked = +this.item.star;
   }
-
+  counter() {
+    let i: number = 5;
+    return new Array( i );
+  }
 }
